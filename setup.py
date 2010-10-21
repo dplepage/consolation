1825e49d88e146b4bd6742557ff5b9fb1d4842e3
Daniel Lepage <dplepage@gmail.com>
2010-10-20 20:03:15 -0400

    Include README, require plac

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from distutils.core import setup
 
 setup(
     name='consolation',
-    version='0.1',
+    version='0.1.1',
     packages=['consolation',],
     license='BSD',
     long_description=open('README.rst').read(),
+    install_requires=['plac'],
 )
