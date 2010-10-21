4fa1175f9200c2d2c9e59302bb3132e4d07ce682
Daniel Lepage <dplepage@gmail.com>
2010-10-21 15:16:01 -0400

    Extend "main" decorator to support plac annotations.

--- consolation/decorate.py
+++ consolation/decorate.py
@@ -10,17 +10,23 @@ show_loops = False
 class MultipleMainWarning(Warning):
     pass
 
-def main(fn):
-    if fn.__module__ == '__main__':
-        if hasattr(main,'_has_target'):
-            warnings.warn(MultipleMainWarning("Too many mains!"))
-        main._has_target=True
-        def call(fn, *args, **kwargs):
-            try:
-                plac.call(fn,*args,**kwargs)
-            except Exception,e:
-                print "{0}: {1}: {2}".format(sys.argv[0], type(e).__name__, e)
-        call(fn)
+def main(*args, **kwargs):
+    def wrapper(fn):
+        fn = plac.annotations(**kwargs)(fn)
+        if fn.__module__ == '__main__':
+            if hasattr(main,'_has_target'):
+                warnings.warn(MultipleMainWarning("Too many mains!"))
+            main._has_target=True
+            def call(fn, *args, **kwargs):
+                try:
+                    plac.call(fn,*args,**kwargs)
+                except Exception,e:
+                    print "{0}: {1}: {2}".format(sys.argv[0], type(e).__name__, e)
+            call(fn)
+    # if it's used with no arguments but a function, call the function
+    if len(kwargs) == 0 and len(args) == 1:
+        return wrapper(args[0])
+    return wrapper
 
 def loop(x):
     if show_loops:
