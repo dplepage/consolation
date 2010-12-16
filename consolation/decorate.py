e54cf999269a37dcfc54f55c3de2c6e704401f4f
Daniel Lepage <dplepage@gmail.com>
2010-12-15 22:01:23 -0500

    Revert gross hacks, leave master in a reasonable state

--- consolation/decorate.py
+++ consolation/decorate.py
@@ -1,50 +1,26 @@
 import sys
-import atexit
-import os
-from itertools import count
 import __builtin__
 from functools import wraps
 import warnings
 import plac
 from progress_bar import progressinfo as pinf
 
-def last_frame():
-    frame = sys._getframe(0)
-    for i in count(1):
-        try:
-            frame = sys._getframe(i)
-        except ValueError:
-            return frame
-
-# TODO is it always the case that the last frame, as of importing this module,
-# is always the file that was actually called? Probably there are toolkits 
-# with their own magic frame manipulation that will break this.
-_main_frame = last_frame() 
-
-#TODO hack this to use names of functions instead of functions themselves.
-
 show_loops = False
 
 class MultipleMainWarning(Warning):
     pass
 
-def main(*args, **kwargs):
-    def wrapper(fn):
-        fn = plac.annotations(**kwargs)(fn)
-        if fn.__module__ == '__main__':
-            if hasattr(main,'_has_target'):
-                warnings.warn(MultipleMainWarning("Too many mains!"))
-            main._has_target=True
-            def call(fn, *args, **kwargs):
-                try:
-                    plac.call(fn,*args,**kwargs)
-                except Exception,e:
-                    print "{0}: {1}: {2}".format(sys.argv[0], type(e).__name__, e)
-            call(fn)
-    # if it's used with no arguments but a function, call the function
-    if len(kwargs) == 0 and len(args) == 1:
-        return wrapper(args[0])
-    return wrapper
+def main(fn):
+    if fn.__module__ == '__main__':
+        if hasattr(main,'_has_target'):
+            warnings.warn(MultipleMainWarning("Too many mains!"))
+        main._has_target=True
+        def call(fn, *args, **kwargs):
+            try:
+                plac.call(fn,*args,**kwargs)
+            except Exception,e:
+                print "{0}: {1}: {2}".format(sys.argv[0], type(e).__name__, e)
+        call(fn)
 
 def loop(x):
     if show_loops:
@@ -64,16 +40,9 @@ def showprogress(fn):
 import time
 
 subs = []
-def subcommand(*args, **kwargs):
-    def wrapper(fn):
-        fn = plac.annotations(**kwargs)(fn)
-        subs.append(fn)
-        return fn
-    # for the @subcommand case, instead of @subcommand(args)
-    if len(kwargs) == 0 and len(args) == 1:
-        return wrapper(args[0])
-    return wrapper
-
+def subcommand(fn):
+    subs.append(fn)
+    return fn
 
 def run_subcommands():
     class Foo(object):
